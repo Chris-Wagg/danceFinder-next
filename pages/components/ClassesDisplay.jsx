985b370a84d60ba6c1281fff258b05717a9d30a7
Chris-Wagg <cgwagg@hotmail.co.nz>
2021-11-01 10:37:58 +1300

    card drop down added

--- pages/components/ClassesDisplay.jsx
+++ pages/components/ClassesDisplay.jsx
@@ -1,3 +1,5 @@
+import React, { useState, useEffect } from 'react'
+
 import {
 	Box,
 	Button,
@@ -6,10 +8,30 @@ import {
 	CardContent,
 	CardMedia,
 	Grid,
-	Typography
+	Typography,
+	styled,
+	IconButton,
+	Collapse
 } from '@mui/material'
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore'
+
+const ExpandMore = styled((props) => {
+	const { expand, ...other } = props
+	return <IconButton {...other} />
+})(({ theme, expand }) => ({
+	transform: !expand ? 'rotate(0deg)' : 'rotate(180deg)',
+	marginLeft: 'auto',
+	transition: theme.transitions.create('transform', {
+		duration: theme.transitions.duration.shortest
+	})
+}))
 
 export default function ClassesDisplay() {
+	const [expanded, setExpanded] = useState(false)
+
+	const handleExpandClick = () => {
+		setExpanded(!expanded)
+	}
 	return (
 		<>
 			<Box
@@ -17,6 +39,7 @@ export default function ClassesDisplay() {
 				flexDirection='row'
 				justifyContent='center'
 				flexWrap='wrap'>
+				{/*  */}
 				<Card lg={4} sx={{ margin: '20px', minWidth: '300px' }}>
 					<CardMedia component='img' height='140' image='#' alt='studio logo' />
 					<CardContent>
@@ -27,9 +50,41 @@ export default function ClassesDisplay() {
 					</CardContent>
 					<CardActions>
 						<Button size='small'>Go to website</Button>
+						<ExpandMore
+							expand={expanded}
+							onClick={handleExpandClick}
+							aria-expanded={expanded}
+							aria-label='show more'>
+							<ExpandMoreIcon />
+						</ExpandMore>
 					</CardActions>
+					<Collapse in={expanded} timeout='auto' unmountOnExit>
+						<CardContent>
+							<Typography variant='h5'>Class types offered:</Typography>
+							<Typography>Jazz</Typography>
+							<Typography>Tap</Typography>
+							<Typography>HipHop</Typography>
+							<Typography>Lyrical</Typography>
+							<Typography>Ballet</Typography>
+							<Typography variant='h5'>Levels offered:</Typography>
+							<Typography variant='h6'>School age:</Typography>
+							<Typography>pre-school</Typography>
+							<Typography>Beginner</Typography>
+							<Typography>Intermediate</Typography>
+							<Typography>Advanced</Typography>
+							<Typography>Professional</Typography>
+							<Typography variant='h6'>Adult classes:</Typography>
+							<Typography>Beginner</Typography>
+							<Typography>Intermediate</Typography>
+							<Typography>Advanced</Typography>
+							<Typography>Professional</Typography>
+							<Typography variant='h6'>Open classes:</Typography>
+							<Typography>School age</Typography>
+							<Typography>Adult</Typography>
+						</CardContent>
+					</Collapse>
 				</Card>
-
+				{/*  */}
 				<Card lg={4} sx={{ margin: '20px', minWidth: '300px' }}>
 					<CardMedia component='img' height='140' image='#' alt='studio logo' />
 					<CardContent>
@@ -40,9 +95,41 @@ export default function ClassesDisplay() {
 					</CardContent>
 					<CardActions>
 						<Button size='small'>Go to website</Button>
+						<ExpandMore
+							expand={expanded}
+							onClick={handleExpandClick}
+							aria-expanded={expanded}
+							aria-label='show more'>
+							<ExpandMoreIcon />
+						</ExpandMore>
 					</CardActions>
+					<Collapse in={expanded} timeout='auto' unmountOnExit>
+						<CardContent>
+							<Typography variant='h5'>Class types offered:</Typography>
+							<Typography>Jazz</Typography>
+							<Typography>Tap</Typography>
+							<Typography>HipHop</Typography>
+							<Typography>Lyrical</Typography>
+							<Typography>Ballet</Typography>
+							<Typography variant='h5'>Levels offered:</Typography>
+							<Typography variant='h6'>School age:</Typography>
+							<Typography>pre-school</Typography>
+							<Typography>Beginner</Typography>
+							<Typography>Intermediate</Typography>
+							<Typography>Advanced</Typography>
+							<Typography>Professional</Typography>
+							<Typography variant='h6'>Adult classes:</Typography>
+							<Typography>Beginner</Typography>
+							<Typography>Intermediate</Typography>
+							<Typography>Advanced</Typography>
+							<Typography>Professional</Typography>
+							<Typography variant='h6'>Open classes:</Typography>
+							<Typography>School age</Typography>
+							<Typography>Adult</Typography>
+						</CardContent>
+					</Collapse>
 				</Card>
-
+				{/*  */}
 				<Card lg={4} sx={{ margin: '20px', minWidth: '300px' }}>
 					<CardMedia component='img' height='140' image='#' alt='studio logo' />
 					<CardContent>
@@ -53,31 +140,39 @@ export default function ClassesDisplay() {
 					</CardContent>
 					<CardActions>
 						<Button size='small'>Go to website</Button>
+						<ExpandMore
+							expand={expanded}
+							onClick={handleExpandClick}
+							aria-expanded={expanded}
+							aria-label='show more'>
+							<ExpandMoreIcon />
+						</ExpandMore>
 					</CardActions>
-				</Card>
-				<Card lg={4} sx={{ margin: '20px', minWidth: '300px' }}>
-					<CardMedia component='img' height='140' image='#' alt='studio logo' />
-					<CardContent>
-						<Typography gutterbloom variant='h3'>
-							Studio Name
-						</Typography>
-						<Typography variant='h5'>this is studio information</Typography>
-					</CardContent>
-					<CardActions>
-						<Button size='small'>Go to website</Button>
-					</CardActions>
-				</Card>
-				<Card lg={4} sx={{ margin: '20px', minWidth: '300px' }}>
-					<CardMedia component='img' height='140' image='#' alt='studio logo' />
-					<CardContent>
-						<Typography gutterbloom variant='h3'>
-							Studio Name
-						</Typography>
-						<Typography variant='h5'>this is studio information</Typography>
-					</CardContent>
-					<CardActions>
-						<Button size='small'>Go to website</Button>
-					</CardActions>
+					<Collapse in={expanded} timeout='auto' unmountOnExit>
+						<CardContent>
+							<Typography variant='h5'>Class types offered:</Typography>
+							<Typography>Jazz</Typography>
+							<Typography>Tap</Typography>
+							<Typography>HipHop</Typography>
+							<Typography>Lyrical</Typography>
+							<Typography>Ballet</Typography>
+							<Typography variant='h5'>Levels offered:</Typography>
+							<Typography variant='h6'>School age:</Typography>
+							<Typography>pre-school</Typography>
+							<Typography>Beginner</Typography>
+							<Typography>Intermediate</Typography>
+							<Typography>Advanced</Typography>
+							<Typography>Professional</Typography>
+							<Typography variant='h6'>Adult classes:</Typography>
+							<Typography>Beginner</Typography>
+							<Typography>Intermediate</Typography>
+							<Typography>Advanced</Typography>
+							<Typography>Professional</Typography>
+							<Typography variant='h6'>Open classes:</Typography>
+							<Typography>School age</Typography>
+							<Typography>Adult</Typography>
+						</CardContent>
+					</Collapse>
 				</Card>
 			</Box>
 		</>
--- pages/components/ClassesPage.jsx
+++ pages/components/ClassesPage.jsx
@@ -17,15 +17,12 @@ export default function LandingPage() {
 						<Grid item lg={2} md={2} sm={2}>
 							<LeftNav />
 						</Grid>
-						<Grid item lg={1} md={2} sm={2}>
-							<h1>this will be the search parameters row</h1>
-						</Grid>
-						<Grid item lg={6} md={5} sm={5}>
+						<Grid item lg={7} md={7} sm={7}>
 							<ClassesDisplay />
 							{/* This might need to change depending on the navigation */}
 						</Grid>
 						<Grid item lg={3} md={3} sm={3}>
-							<RightNav />
+							<h1>this will be the search parameters row</h1>
 						</Grid>
 					</Grid>
 				</Box>
