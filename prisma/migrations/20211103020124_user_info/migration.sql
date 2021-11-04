2d632fecc2e7f829e7968108ba6b4ee5d237cd72
Chris-Wagg <cgwagg@hotmail.co.nz>
2021-11-04 16:15:31 +1300

    prisma added

---
+++ prisma/migrations/20211103020124_user_info/migration.sql
@@ -0,0 +1,8 @@
+-- CreateTable
+CREATE TABLE "userInfo" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "name" TEXT NOT NULL,
+    "userName" TEXT NOT NULL,
+    "emailAddress" TEXT NOT NULL,
+    "avatar" TEXT NOT NULL
+);
