4b847622c8f88d26fd6b853ad671123aeeb1213f
Chris-Wagg <cgwagg@hotmail.co.nz>
2021-11-09 18:50:13 +1300

    styling added

--- pages/components/LeftNav.jsx
+++ pages/components/LeftNav.jsx
@@ -1,16 +1,28 @@
 import Link from 'next/link'
 
-import AccessTimeIcon from '@mui/icons-material/AccessTime'
 import { Button, Box, Grid } from '@mui/material'
 import { makeStyles } from '@material-ui/core'
+import { createTheme, ThemeProvider } from '@mui/material'
+
+import AccessTimeIcon from '@mui/icons-material/AccessTime'
+import HomeOutlinedIcon from '@mui/icons-material/HomeOutlined'
 
 const useStyle = makeStyles((theme) => {
 	return {
 		navBox: {
-			// background: '#090909',
-			border: '5px solid black',
-			// width: '30vw',
-			margin: '1%'
+			background: '#1d1d1d',
+			border: '2px solid black',
+			margin: '1%',
+			borderRadius: '30px',
+			color: 'white'
+		}
+	}
+})
+
+const theme = createTheme({
+	palette: {
+		primary: {
+			main: '#ffffff'
 		}
 	}
 })
@@ -18,55 +30,60 @@ const useStyle = makeStyles((theme) => {
 export default function LeftNav() {
 	const classes = useStyle()
 	return (
-		<Box>
-			<Grid
-				container
-				rowSpacing={4}
-				direction='column'
-				alignItems='center'
-				className={classes.navBox}>
-				<Grid item rowSpacing={0}>
-					<h1>danceFinder</h1>
-				</Grid>
-				<Grid item />
-				<Grid item />
-				<Grid item>
-					<Button variant='contained' startIcon={<AccessTimeIcon />}>
-						<Link href='/'>Home</Link>
-					</Button>
-				</Grid>
-				<Grid item>
-					<Button variant='contained' startIcon={<AccessTimeIcon />}>
-						<Link href='/components/ClassesPage'>Find classes</Link>
-						{/* fix this link later to makes sure components doesn't show in the url */}
-					</Button>
-				</Grid>
-				<Grid item>
-					<Button variant='contained' startIcon={<AccessTimeIcon />}>
-						find Events
-					</Button>
-				</Grid>
-				<Grid item>
-					<Button variant='contained' startIcon={<AccessTimeIcon />}>
-						Find media
-					</Button>
-				</Grid>
-				<Grid item>
-					<Button variant='contained' startIcon={<AccessTimeIcon />}>
-						Online resources
-					</Button>
-				</Grid>
+		<ThemeProvider theme={theme}>
+			<Box>
+				<Grid
+					container
+					rowSpacing={4}
+					direction='column'
+					alignItems='center'
+					className={classes.navBox}>
+					<Grid item rowSpacing={0}>
+						<h1>danceFinder</h1>
+					</Grid>
+					<Grid item />
+					{/* <Grid item /> */}
+					<Grid item sx={{ alignItems: 'stretch' }}>
+						<Button
+							color='primary'
+							variant='outlined'
+							startIcon={<HomeOutlinedIcon />}>
+							<Link href='/'>Home</Link>
+						</Button>
+					</Grid>
+					<Grid item>
+						<Button variant='outlined' startIcon={<AccessTimeIcon />}>
+							<Link href='/components/ClassesPage'>classes</Link>
+							{/* fix this link later to makes sure components doesn't show in the url */}
+						</Button>
+					</Grid>
+					<Grid item>
+						<Button variant='outlined' startIcon={<AccessTimeIcon />}>
+							Events
+						</Button>
+					</Grid>
+					<Grid item>
+						<Button variant='outlined' startIcon={<AccessTimeIcon />}>
+							Find media
+						</Button>
+					</Grid>
+					<Grid item>
+						<Button variant='outlined' startIcon={<AccessTimeIcon />}>
+							Online resources
+						</Button>
+					</Grid>
 
-				<Grid item />
-				<Grid item />
-				<Grid item />
+					<Grid item />
+					<Grid item />
+					<Grid item />
 
-				<Grid item sx={{ marginBottom: '50px' }}>
-					<Button variant='contained' startIcon={<AccessTimeIcon />}>
-						Account stuff here
-					</Button>
+					<Grid item sx={{ marginBottom: '50px' }}>
+						<Button variant='outlined' startIcon={<AccessTimeIcon />}>
+							Account stuff here
+						</Button>
+					</Grid>
 				</Grid>
-			</Grid>
-		</Box>
+			</Box>
+		</ThemeProvider>
 	)
 }
--- pages/components/RightNav.jsx
+++ pages/components/RightNav.jsx
@@ -1,40 +1,94 @@
 import { Button, Box, Grid } from '@mui/material'
+import { makeStyles } from '@material-ui/core'
+import { createTheme, ThemeProvider } from '@mui/material'
+
 import AccessTimeIcon from '@mui/icons-material/AccessTime'
 
+const useStyle = makeStyles((theme) => {
+	return {
+		rightNav: {
+			background: '#1d1d1d',
+			border: '2px solid black',
+			margin: '1%',
+			borderRadius: '30px',
+			color: 'white'
+		},
+		button: {
+			backgroundColor: 'white'
+		}
+	}
+})
+
+const theme = createTheme({
+	palette: {
+		primary: {
+			main: '#ffffff'
+		}
+	}
+})
+
 export default function RightNav() {
+	const classes = useStyle()
 	return (
-		<>
-			<Box
-				display='flex'
-				flexDirection='row'
-				justifyContent='space-evenly'
-				alignItems='center'
-				spacing={3}>
-				<Button
-					variant='contained'
-					startIcon={<AccessTimeIcon />}
-					sx={{ margin: '10px' }}>
-					my Timetable
-				</Button>
+		<ThemeProvider theme={theme}>
+			<>
+				<Box className={classes.rightNav}>
+					<Grid
+						container
+						direction='row'
+						justifyContent='center'
+						columnSpacing={2}>
+						<Grid item>
+							<Button
+								variant='outlined'
+								startIcon={<AccessTimeIcon />}
+								sx={{ margin: '10px' }}>
+								my Timetable
+							</Button>
+						</Grid>
 
-				<Button
-					variant='contained'
-					startIcon={<AccessTimeIcon />}
-					sx={{ margin: '10px' }}>
-					my Events
-				</Button>
+						<Grid item>
+							<Button
+								variant='outlined'
+								startIcon={<AccessTimeIcon />}
+								sx={{ margin: '10px' }}>
+								my Events
+							</Button>
+						</Grid>
+					</Grid>
 
-				{/* <Button
-					variant='contained'
+					{/* </Box> */}
+					<Box>
+						<h1>right nav content here in a box test wrap</h1>
+						<Button
+							variant='outlined'
+							startIcon={<AccessTimeIcon />}
+							sx={{ margin: '10px' }}>
+							add thing
+						</Button>
+					</Box>
+					{/* </Grid> */}
+				</Box>
+			</>
+		</ThemeProvider>
+	)
+}
+
+{
+	/* <Button
+					variant='outlined'
 					startIcon={<AccessTimeIcon />}
 					sx={{ margin: '10px' }}>
 					Messages
-				</Button> */}
-			</Box>
+				</Button> */
+}
 
-			<Box>
-				<h1>right nav content here in a box test wrap</h1>
-			</Box>
-		</>
-	)
+{
+	/* <Box
+							display='flex'
+							flexDirection='row'
+							justifyContent='space-evenly'
+							alignItems='center'
+							spacing={3}
+							className={classes.rightNav}> */
 }
